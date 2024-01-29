7e93309802d97db89371b5f756df01b1368c3ab4
djdjz7 <djdjz7@icloud.com>
2024-01-29 21:50:55 +0800

    that's all for today :)

---
+++ OpenFlierWebshot/WebshotPlugin.cs
@@ -0,0 +1,34 @@
+﻿using OpenFlier.Plugin;
+using Microsoft.Playwright;
+
+namespace OpenFlierWebshot
+{
+    public class WebshotPlugin : ICommandInputPlugin
+    {
+        public static IPlaywright? PlaywrightEnv { get; set; }
+
+        public CommandInputPluginInfo GetPluginInfo()
+            => new CommandInputPluginInfo()
+            {
+                InvokeCommands = new[] { "aws", "ws" },
+                PluginAuthor = "djdjz7",
+                PluginDescription = "Takes a screenshot of a webpage.",
+                PluginIdentifier = "openflier.djdjz7.webshot",
+                PluginName = "OpenFlier Webshot",
+                PluginNeedsAdminPrivilege = false,
+                PluginNeedsConfigEntry = false,
+                PluginVersion = "1.0.0",
+            };
+
+        public async Task PluginMain(CommandInputPluginArgs args)
+        {
+            if (PlaywrightEnv is null)
+                PlaywrightEnv = await Playwright.CreateAsync();
+        }
+
+        public void PluginOpenConfig()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
