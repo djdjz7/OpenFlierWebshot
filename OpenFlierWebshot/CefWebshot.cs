595babc39bdd594b12bc408ef4b7714c8e948042
Jason Dai <djdjz7@icloud.com>
2024-03-03 09:20:24 +0800

    finish

---
+++ OpenFlierWebshot/CefWebshot.cs
@@ -0,0 +1,61 @@
+﻿using CefSharp;
+using CefSharp.DevTools.Page;
+using CefSharp.OffScreen;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace CloudRoom.MqttModule
+{
+    public class CefWebshot
+    {
+        public static async Task<Bitmap> TakeWebshot(string url,int? width, int? height)
+        {
+            if (string.IsNullOrWhiteSpace(url))
+                return new Bitmap(Stream.Null);
+            try
+            {
+                var browser = new ChromiumWebBrowser(url);
+                await browser.WaitForInitialLoadAsync();
+                var cefbrowserHost = browser.GetBrowserHost();
+                if (url.Contains("moegirl.org.cn"))
+                {
+                    browser.ExecuteScriptAsync(@"window.localStorage.setItem(""moeskin:sitenotice-lastview-content"",""萌娘百科衷心希望新冠感染疫情早日结束！关于萌娘百科站内广告的说明为了拯救萌百，维持运营开发和服务器运转，我们近期在站内添加了一批广告。同时，在引入了“延伸确认用户”用户组后，我们也为组内的用户免除了绝大部分广告。……由于此次涉及到多个广告发布商，且各个供应商提供的广告种类较多且杂，对于广告所展示的内容，还请大家仔细甄别。对于突如其来的弹窗等广告，可选择直接关闭。……感谢大家一直以来对萌娘百科的支持与理解。阅读更多公告关于技术性故障的公告讨论中事项设立分类指引与讨论专案的提案"")");
+                    browser.ExecuteScriptAsync(@"window.localStorage.setItem(""moeskin:sitenotice/last-viewed"",""萌娘百科会员服务(试行)公告登录用户即可免费领取会员服务公告关于全站半保护的公告关于萌娘百科近期改革的公告近期人事调整规则及其变动之公告关于萌娘百科政策更改事宜的公告讨论中事项2023年政策修订增补专案关于软硬件收录范围的修正案"")");
+                }
+                cefbrowserHost.Invalidate(PaintElementType.View);
+                var contentSize = await browser.GetContentSizeAsync();
+                var viewport = new Viewport();
+                if (width == null || height == null)
+                {
+                    viewport = new Viewport
+                    {
+                        Height = contentSize.Height,
+                        Width = contentSize.Width,
+                        Scale = 1.0
+                    };
+                }
+                else
+                {
+                    viewport = new Viewport
+                    {
+                        Height = (double)height,
+                        Width = (double)width,
+                        Scale = 1.0
+                    };
+                }
+                var buffer = await browser.CaptureScreenshotAsync(CaptureScreenshotFormat.Png, 100, viewport);
+                var ms = new MemoryStream(buffer);
+                Bitmap bitmap = new Bitmap(ms);
+                if (!browser.IsDisposed)
+                    browser.Dispose();
+                return bitmap;
+            }
+            catch (Exception ex)
+            {
+                return new Bitmap(Stream.Null);
+            }
+        }
+    }
+}
--- OpenFlierWebshot/WebshotPlugin.cs
+++ OpenFlierWebshot/WebshotPlugin.cs
@@ -1,16 +1,24 @@
 ﻿using OpenFlier.Plugin;
-using Microsoft.Playwright;
+using CloudRoom.MqttModule;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.Text;
+using System.Windows;
+using Newtonsoft.Json;
+using MQTTnet;
+using MQTTnet.Protocol;
+using MQTTnet.Server;
+using CefSharp;
+using CefSharp.OffScreen;
 
 namespace OpenFlierWebshot
 {
     public class WebshotPlugin : ICommandInputPlugin
     {
-        public static IPlaywright? PlaywrightEnv { get; set; }
-
         public CommandInputPluginInfo GetPluginInfo()
             => new CommandInputPluginInfo()
             {
-                InvokeCommands = new[] { "aws", "ws" },
+                InvokeCommands = new[] { "aws", "ws", "bs" },
                 PluginAuthor = "djdjz7",
                 PluginDescription = "Takes a screenshot of a webpage.",
                 PluginIdentifier = "openflier.djdjz7.webshot",
@@ -22,13 +30,89 @@ namespace OpenFlierWebshot
 
         public async Task PluginMain(CommandInputPluginArgs args)
         {
-            if (PlaywrightEnv is null)
-                PlaywrightEnv = await Playwright.CreateAsync();
+            if (!Cef.IsInitialized)
+            {
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    Cef.Initialize(new CefSettings() { BackgroundColor = 0x00ffffff, Locale = "zh-CN" });
+                    ;
+                });
+            }
+            var filename = Guid.NewGuid().ToString("N") + (args.UsePng ? ".png" : ".jpeg");
+            var fullcmd = args.FullCommand;
+            switch (args.InvokeCommand.ToLower())
+            {
+                case "ws":
+                    if (fullcmd.Split(' ').Length == 1)
+                        await TakeWebshot($"Screenshots\\{filename}", "https://bilibili.com");
+                    else
+                        await TakeWebshot($"Screenshots\\{filename}", fullcmd.Split(' ')[1]);
+                    break;
+
+                case "aws":
+                    if (fullcmd.Split(' ').Length <= 3)
+                        throw new Exception("缺少分辨率");
+                    else
+                    {
+                        string[] cmdlist = fullcmd.Split(' ');
+                        int width = int.Parse(cmdlist[1]);
+                        int height = int.Parse(cmdlist[2]);
+                        string url = string.Join(' ', cmdlist, 3, cmdlist.Length - 3);
+                        await TakeWebshot($"Screenshots\\{filename}", url, width, height);
+                    }
+                    break;
+
+                case "bs":
+                    if (fullcmd.Split(' ', StringSplitOptions.RemoveEmptyEntries).Length <= 1)
+                        throw new Exception("必须输入搜素内容");
+                    string searchContent = string.Join(' ', fullcmd.Split(' ', StringSplitOptions.RemoveEmptyEntries), 1, fullcmd.Split(' ', StringSplitOptions.RemoveEmptyEntries).Length - 1);
+                    await TakeWebshot($"Screenshots\\{filename}", $"https://bing.com/search?q={Uri.EscapeDataString(searchContent)}", 1920, 2400);
+                    break;
+            }
+
+            string payload = JsonConvert.SerializeObject(
+                new
+                {
+                    type = 20005L,
+                    data = new
+                    {
+                        name = filename,
+                        deviceCode = args.MachineIdentifier,
+                        versionCode = args.Version,
+                    }
+                }
+            );
+            await args.MqttServer.PublishAsync(
+                new MqttApplicationMessage
+                {
+                    Topic = args.ClientID + "/REQUEST_SCREEN_CAPTURE",
+                    Payload = Encoding.Default.GetBytes(payload),
+                    QualityOfServiceLevel = MqttQualityOfServiceLevel.ExactlyOnce
+                }
+            );
         }
 
         public void PluginOpenConfig()
         {
             throw new NotImplementedException();
+        }
+
+        public async static Task TakeWebshot(string filename, string url, int? width = null, int? height = null)
+        {
+
+            Bitmap WebScreenshotBitmap = await CefWebshot.TakeWebshot(url, width, height);
+            if (filename.EndsWith("png", StringComparison.OrdinalIgnoreCase))
+                WebScreenshotBitmap.Save(filename, ImageFormat.Png);
+            else
+                WebScreenshotBitmap.Save(filename, ImageFormat.Jpeg);
+        }
+
+        public async Task BeforeExit()
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                Cef.Shutdown();
+            });
         }
     }
 }
